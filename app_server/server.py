49860edfa812ff4a8997553d08e8d54a647d4846
Ubuntu <gaya.0408@gmail.com>
2014-02-27 19:14:16 +0000

    Changes to the app server

--- app_server/server.py
+++ app_server/server.py
@@ -3,6 +3,7 @@ from flask import *
 import requests
 import db_util
 import json
+import urllib2
 app = Flask(__name__)
 API_KEY = 'key=AIzaSyAj-OXTDcDJhJBLbc7XNVfkHqQYNMnIn0w'
 
@@ -18,14 +19,15 @@ def registration():
 	db_util.register_device(regid,phone_number)
 	return "Hello"
 
-@app.route('/send_msg_to_server')
+@app.route('/send_message')
 def send_msg_to_server():
-	message = request.args.get('message',None)
-	phone_number = request.args.get('phone_number',None) 
-	print phone_number, message, API_KEXY
-	regid = db_util.fetch_registration_id(phone_number)
+	message = urllib2.unquote(request.args.get('message',None))
+	sender_number = request.args.get('sender_number',None) 
+	receiver_number = request.args.get('receiver_number',None) 
+	print "sender ::",sender_number,"receiver ::",receiver_number,"message :",message, API_KEY
+	regid = db_util.fetch_registration_id(receiver_number)
 	url = 'https://android.googleapis.com/gcm/send'
-	payload = {'registration_ids' : [ regid ] , 'data' : { 'message' : message } }
+	payload = {'registration_ids' : [ regid ] , 'data' : { 'message' : message, 'sender' : sender_number } }
 	headers = {'Content-Type': 'application/json' , 'Authorization' : API_KEY}
 	print "-----------------------------------------------------------"
 	r = requests.post(url, data = json.dumps(payload), headers = headers)
